04db78c84963db51dab5ddbf658cf368ca80d209
retroduck385 <retroduck385@gmail.com>
2025-07-17 08:47:36 +0800

    Fixed if statement

--- LOVELink/routes/organizationActivitiesRoute.js
+++ LOVELink/routes/organizationActivitiesRoute.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const activitiesDataModule = require('../model/organizationActivitiesController.js');
+const membersDataModule = require('../model/membersController.js');
 
 
 router.get('/organization-activities', async (req, res) => {
    try {
+        const email = "albrecht_abad@dlsu.edu.ph"; // update to user session
+        const userData = await membersDataModule.getUser(email);
         const events = await activitiesDataModule.get_all_activities();
         console.log(events);
 
         res.render('pages/view-organization-activities', {
             title: 'Organization Activities',
             styles: '<link rel="stylesheet" href="/css/Activities.css">',
-            events: events
+            events: events,
+            user: userData
         });
     } catch (err) {
         console.error(err);
