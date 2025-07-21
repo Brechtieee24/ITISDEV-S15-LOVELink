b56ca53a25cc00e803ed6560d4f8af6a36ea49af
retroduck385 <retroduck385@gmail.com>
2025-07-22 03:19:55 +0800

    No Change as of the moment

--- LOVELink/model/activityParticipationsController.js
+++ LOVELink/model/activityParticipationsController.js
@@ -33,7 +33,30 @@ async function getEventsOfUser(memberId) {
     }
 }
 
+// async function calculateTotalHours(events){
+
+//     totalMS = 0;
+
+//     for (i = 0; i < len(events); i++){
+//         const timeIn = new Date(events[i].timeIn);
+//         const timeOut = new Date(events[i].timeOut);
+//         difference = timeIn - timeOut;
+//         totalMS += difference; 
+//     }
+
+//     const totalSeconds = Math.floor(ms / 1000);
+//     const hours = Math.floor(totalSeconds / 3600);
+//     const minutes = Math.floor((totalSeconds % 3600) / 60);
+//     const seconds = totalSeconds % 60;
+
+//     const pad = (num) => String(num).padStart(2, '0');
+
+//     return ${pad(hours)}:${pad(minutes)}:${pad(seconds)};
+//     return total_time / 1000; //Gets the total seconds
+// }
+
 module.exports = {
    addEventParticipation,
-   getEventsOfUser
+   getEventsOfUser,
+   getTotalHours,
 };
