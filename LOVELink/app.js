91125e11b7576b116c94626fbc266b6d0e79bfa7
[Albrecht Gabriel] <[albrecht_abad@dlsu.edu.ph]>
2025-07-25 17:45:38 +0800

    images

--- LOVELink/app.js
+++ LOVELink/app.js
@@ -93,7 +93,6 @@ app.use((req, res, next) => {
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 hbs.registerHelper('eq', (a, b) => a === b);
 hbs.registerHelper('concatClass', (baseClass, condition, conditionalClass) =>
